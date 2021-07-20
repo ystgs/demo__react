bfa7e89f30dae0a0437e992f36b225a23feb6fb8
yoshitogase <togamichi0522@gmail.com>
2021-07-21 01:11:56 +0900

    ✨ react

--- src/index.js
+++ src/index.js
@@ -12,15 +12,21 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square" onClick={() => this.setState({ value: 'X' })}>
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
