d5129c105e89a093a935d3679b0f3d585c1745e9
yoshitogase <togamichi0522@gmail.com>
2021-07-24 18:36:21 +0900

    ✨ make board working

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square" onClick={() => this.setState({ value: 'X' })}>
-        {this.props.value}
+        {this.state.value}
       </button>
     );
   }
